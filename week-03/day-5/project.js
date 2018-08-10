e3cff5b5cf7e09cd8a68eaa7e5274eba2f5eb861
Allan Cai <Allan_Cai@epam.com>
2018-08-10 17:43:41 +0800

    project second push

--- week-03/day-5/project.js
+++ week-03/day-5/project.js
@@ -1,3 +1,18 @@
+const imgButton1 = document.getElementsByClassName('small')[0];
+const largeImg1 = document.getElementById('largeImg1');
+
+const imgButton2 = document.getElementsByClassName('small')[1];
+const largeImg2 = document.getElementById('largeImg2');
+
+const imgButton3 = document.getElementsByClassName('small')[2];
+const largeImg3 = document.getElementById('largeImg3');
+
+const imgButton4 = document.getElementsByClassName('small')[3];
+const largeImg4 = document.getElementById('largeImg4');
+
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -14,27 +29,59 @@ function showDivs(n) {
      x[i].style.display = "none";
   }
   x[slideIndex-1].style.display = "block";
+
+  const h1 = document.querySelector('h1');
+    if(largeImg4.style.display === 'block'){
+    h1.innerText = 'This is the fourth Image!';
+    p.innerText = '444';
+    imgButton4.style.border = '5px solid red';
+    imgButton1.style.border = '5px solid white';
+    imgButton2.style.border = '5px solid white';
+    imgButton3.style.border = '5px solid white';
+    }else if(largeImg1.style.display === 'block'){
+      h1.innerText = 'This is the first Image!';
+      p.innerText = '111';
+      imgButton1.style.border = '5px solid red';
+      imgButton2.style.border = '5px solid white';
+      imgButton3.style.border = '5px solid white';
+      imgButton4.style.border = '5px solid white';
+    }else if(largeImg2.style.display === 'block'){
+      h1.innerText = 'This is the second Image!';
+      p.innerText = '222';
+      imgButton2.style.border = '5px solid red';
+      imgButton1.style.border = '5px solid white';
+      imgButton3.style.border = '5px solid white';
+      imgButton4.style.border = '5px solid white';
+    }else if(largeImg3.style.display === 'block'){
+      h1.innerText = 'This is the third Image!';
+      p.innerText = '333';
+      imgButton3.style.border = '5px solid red';
+      imgButton1.style.border = '5px solid white';
+      imgButton2.style.border = '5px solid white';
+      imgButton4.style.border = '5px solid white';
+    }
 }
 
-const imgButton1 = document.getElementsByClassName('small')[0];
-const largeImg1 = document.getElementById('largeImg1');
 
-const imgButton2 = document.getElementsByClassName('small')[1];
-const largeImg2 = document.getElementById('largeImg2');
+//  console.log(largeImg4);
+//  console.log(largeImg4.style.display);
+ if(largeImg4.style.display === 'block'){
+   h1.innerText = 'This is the fourth Image!';
+ }
 
-const imgButton3 = document.getElementsByClassName('small')[2];
-const largeImg3 = document.getElementById('largeImg3');
 
-const imgButton4 = document.getElementsByClassName('small')[3];
-const largeImg4 = document.getElementById('largeImg4');
-
-const h1 = document.querySelector('h1');
 
 imgButton1.addEventListener('click', function() {
   largeImg1.setAttribute('style', 'display: block;');
   largeImg2.setAttribute('style', 'display: none;');
   largeImg3.setAttribute('style', 'display: none;');
   largeImg4.setAttribute('style', 'display: none;');
+  h1.innerText = 'This is the first Image!';
+  p.innerText = '111';
+  imgButton1.style.border = '5px solid red';
+  imgButton2.style.border = '5px solid white';
+  imgButton3.style.border = '5px solid white';
+  imgButton4.style.border = '5px solid white';
 });
 
 imgButton2.addEventListener('click', function() {
@@ -42,7 +89,12 @@ imgButton2.addEventListener('click', function() {
   largeImg1.setAttribute('style', 'display: none;');
   largeImg3.setAttribute('style', 'display: none;');
   largeImg4.setAttribute('style', 'display: none;');
-  h1.innerText = 'This is the second Image!'
+  h1.innerText = 'This is the second Image!';
+  p.innerText = '222';
+  imgButton2.style.border = '5px solid red';
+  imgButton1.style.border = '5px solid white';
+  imgButton3.style.border = '5px solid white';
+  imgButton4.style.border = '5px solid white';
 });
 
 imgButton3.addEventListener('click', function() {
@@ -50,6 +102,12 @@ imgButton3.addEventListener('click', function() {
   largeImg1.setAttribute('style', 'display: none;');
   largeImg2.setAttribute('style', 'display: none;');
   largeImg4.setAttribute('style', 'display: none;');
+  h1.innerText = 'This is the third Image!';
+  p.innerText = '333';
+  imgButton3.style.border = '5px solid red';
+  imgButton2.style.border = '5px solid white';
+  imgButton1.style.border = '5px solid white';
+  imgButton4.style.border = '5px solid white';
 });
 
 imgButton4.addEventListener('click', function() {
@@ -57,4 +115,10 @@ imgButton4.addEventListener('click', function() {
   largeImg1.setAttribute('style', 'display: none;');
   largeImg2.setAttribute('style', 'display: none;');
   largeImg3.setAttribute('style', 'display: none;');
+  h1.innerText = 'This is the fourth Image!';
+  p.innerText = '444';
+  imgButton4.style.border = '5px solid red';
+  imgButton1.style.border = '5px solid white';
+  imgButton2.style.border = '5px solid white';
+  imgButton3.style.border = '5px solid white';
 });
